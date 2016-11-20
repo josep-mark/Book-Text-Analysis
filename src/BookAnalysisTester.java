b073097013a8c5ebe1b4a072e733c0687ad9c7a5
Joseph Franz <Joseph Franz>
2016-11-20 02:25:04 -0500

    Made more changes

--- src/BookAnalysisTester.java
+++ src/BookAnalysisTester.java
@@ -1,5 +1,7 @@
 import java.io.FileNotFoundException;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BookAnalysisTester {
 
@@ -20,11 +22,80 @@ public class BookAnalysisTester {
 //		}
 		
 		WordFrequency words = new WordFrequency(book);
-		ArrayList<String> themWords = words.getAllWords();
-		ArrayList<Integer> count = words.getWordCount();
+//		ArrayList<String> wordList = words.getAllWords();
+//		System.out.println(wordList);
+		
+		ArrayList<String> themWords = words.getMostFrequentWords();
+		ArrayList<Integer> count = words.getCounts();
+		
+//		for (int i = 0; i < wordList.size(); i++){
+//			System.out.println(wordList.get(i));
+//		}
 		for (int i = 0; i < 10; i++){
 			System.out.println(themWords.get(i) + " : " + count.get(i));
 		}
+//		System.out.println(themWords);
+//		System.out.println(count);
+//		System.out.println(themWords);
+//		System.out.println(count);
+//		
+//		ArrayList<String> bookText = new ArrayList<String>();
+//		String file = "alice-in-wonderland.txt";
+//		
+//		try {
+//			File inputFile = new File(file); 
+//			Scanner in = new Scanner(inputFile);
+//				
+//			while (in.hasNextLine()) { 
+//				String line = in.nextLine();
+//				String[] words = line.split(" ");
+//				for (int i = 0; i < words.length; i ++){
+//					String s = words[i].replaceAll("[^a-zA-Z]", "");
+//					bookText.add(s);
+//				}
+//			}
+//				
+//			in.close(); 			 
+//		
+//		}
+//		catch(FileNotFoundException f){
+//			System.out.println("Thats not a book!");
+//		}
+		
+//		for (int i = 0; i < bookText.size(); i++){
+//				String s = bookText.get(i);
+//				s = s.replaceAll("^[a-zA-Z]", "");
+//				s = s.replace("!", "");
+//				s = s.replace("?", "");
+//				s = s.replace(".", "");
+//				s = s.replace(":", "");
+//				s = s.replace(";", "");
+//				s = s.replace("(", "");
+//				s = s.replace(")", "");
+//				s = s.replace("*", "");
+//				s = s.replace(" ", "");
+//				s = s.replace("  ", "");
+//				s = s.replace("\n", "");
+//				s = s.toLowerCase();
+//				if (s.startsWith("\'") | s.endsWith("'")){
+//					s = s.replace("\'", "");
+//				}
+//				bookText.set(i, s);
+//				if (s.isEmpty()){
+//					i = i + 1;
+//					bookText.remove(i - 1);
+//				}
+//				else if (s.startsWith(" ")){
+//					i = i + 1;
+//					bookText.remove(i - 1);
+//				}
+//		}
+		
+//		for (int i = 0; i < bookText.size(); i++){
+//			System.out.println(bookText.get(i));
+//		}
+		
+//		System.out.println('c' - 'a');
 		
 	}
 
--- src/WordFrequency.java
+++ src/WordFrequency.java
@@ -10,6 +10,7 @@ public class WordFrequency {
 	private ArrayList<String> words;
 	private ArrayList<Integer> wordCount;
 	private ArrayList<String> mostFrequent;
+	private ArrayList<Integer> mostFrequentCount;
 	
 	/**
 	 * The constructor for the word frequency class
@@ -19,6 +20,8 @@ public class WordFrequency {
 		bookText = book;
 		words = new ArrayList<String>();
 		wordCount = new ArrayList<Integer>();
+		mostFrequent = new ArrayList<String>();
+		mostFrequentCount = new ArrayList<Integer>();
 		getWords();
 		countWords();
 		order();
@@ -30,11 +33,15 @@ public class WordFrequency {
 	
 	private void getWords(){
 		for (String s : bookText){
-			String[] lineSplit = s.replaceAll("[^a-zA-Z] ", " ").toLowerCase().trim().split(" ");
-			
+			String[] lineSplit = s.split(" ");
 			for (int i = 0; i < lineSplit.length; i++){
-				String text = lineSplit[i];
-				words.add(text);
+				String text = lineSplit[i].replaceAll("[^a-zA-Z]", "");
+				if (text.equals("")){
+					continue;
+				}
+				else {
+					words.add(text);
+				}
 			}	
 		}
 	}
@@ -46,39 +53,87 @@ public class WordFrequency {
 		return words;
 	}
 	
+	/**
+	 * This method counts the words in the word list
+	 */
+	
 	private void countWords(){
 		for (int i = 0; i < words.size(); i++){
 			String word = words.get(i);
 			int count = 0;
-			for (int j = 0; j < words.size(); j++){
-				String test = words.get(j);
-				if (test.equalsIgnoreCase(word)){
-					count ++;
+			if (isFirst(i)){
+				for (int j = 0; j < words.size(); j++){
+					String test = words.get(j);
+					if (test.equalsIgnoreCase(word)){
+						count ++;
+					}
 				}
 			}
 			wordCount.add(count);
 		}
 	}
 	
+	private boolean isFirst(int i){
+		boolean firstOccurence = true;
+		for (int j = 0; j < i; j++){
+			if (words.get(j).equalsIgnoreCase(words.get(i))){
+				firstOccurence = false;
+			}
+		}
+		return firstOccurence;
+		
+	}
+	
+	/**
+	 * Accessor method for the word list
+	 * @return word count Array List of Integers
+	 */
+	
 	public ArrayList<Integer> getWordCount(){
 		return wordCount;
 	}
 	
+	/**
+	 * This method orders the wordCount and words lists by size
+	 * In descending order
+	 */
+	
 	private void order(){
-		for (int i = 0; i < wordCount.size(); i++){
-			for (int j = i + 1; j < wordCount.size() - 1; j++){
+		int q = 0;
+		while (q < 10){
+			String largestWord = "";
+			int largestNum = 0;
+			int index = 1000000;
+			for (int i = 0; i < wordCount.size(); i++){
+				boolean greatest = true;
+				String word = words.get(i);
 				int count = wordCount.get(i);
-				int next = wordCount.get(j);
-				if (next > count){
-					String temp1 = words.get(i);
-					String temp2 = words.get(j);
-					wordCount.set(j, count);
-					wordCount.set(i, next);
-					words.set(j, temp1);
-					words.set(i, temp2);
+				for (int j = 0; j < wordCount.size(); j++){
+					int next = wordCount.get(j);
+					if (next > count){
+						greatest = false;
+					}
+				}
+				if (greatest == true){
+					largestWord = word;
+					largestNum = count;
+					words.remove(i);
+					wordCount.remove(i);
 				}
 			}
-		}	
+			mostFrequent.add(largestWord);
+			mostFrequentCount.add(largestNum);
+//			words.remove(index);
+//			wordCount.remove(index);
+			q++;
+		}
 	}
 	
+	public ArrayList<String> getMostFrequentWords(){
+		return mostFrequent;
+	}
+	
+	public ArrayList<Integer> getCounts(){
+		return mostFrequentCount;
+	}
 }
